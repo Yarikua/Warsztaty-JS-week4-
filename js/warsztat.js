c2be1652d9f196bce6a91a3fd3585b72c69a12bb
Yarikua <mr.yarikua@gmail.com>
2017-04-02 15:37:14 +0200

    Zadanie Ustaw Tło

---
+++ js/warsztat.js
@@ -0,0 +1,17 @@
+'use strict'
+
+
+function ustawTlo(){
+    var docBody = document.getElementById("pierwszy");
+    docBody.style.background="#DC143C" ;
+    var docBody = document.getElementById("drugi");
+    docBody.style.background="#7FFF00" ;
+}
+document.getElementById("przycisk").onclick = ustawTlo;
+
+
+
+
+
+
+
